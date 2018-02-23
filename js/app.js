fee2686b1bfebb79fd3b1413cba89cba800b00c3
James McDaniel <icathaid@gmail.com>
2018-02-23 08:02:56 -0800

    pushing what I have

--- js/app.js
+++ js/app.js
@@ -1,20 +1,31 @@
-var buyStuff = document.getElementById("dropdown");
+var buyStuff = document.getElementById("product");
+var qty = document.getElementById("qty");
+var addToCart = document.getElementById("addToCart");
+var submit = document.getElementById("submit");
+var cart = [];
 buyStuff.addEventListener('change', wellNamedFunction);
+submit.addEventListener('submit', buyMe);
+
 var stuff;
-
-
-var addToCart = [];
+var cart = [];
 function wellNamedFunction(){
-    stuff = 
-    localStorage.setItem("name", JSON.stringify(stuff));
-//    addToCart.push(1);
-    JSON.parse(localStorage.getItem("name"))
-    console.log('bill: ' + addToCart);
+    //recreate if statement matching dropdown selection to index of the array
+    //BuyMe('bag', qty)
+};
+function buyMe(){
+    new BuyMe('bag', qty)
+    console.log(qty);
 };
 
 
+function BuyMe(name, qty){
+    this.name = name;     
+    this.qty = qty;
+    cart.push(this);
+};
+function checkout(){
 
-
+};
 
 function pol(){
     console.log('proof of life');
